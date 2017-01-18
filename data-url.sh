e23327ab9c61310175fc99ee42871ad3b401d87f
Matis Lepik <matis.lepik@gmail.com>
2017-01-19 01:22:46 +0200

    Move overlay data url to separate file

--- data-url.sh
+++ data-url.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mimetype=$(file -bN --mime-type "overlay.png")
 content=$(base64 -w0 < "overlay.png")
-output="const OVERLAY_URL = 'data:$mimetype;base64,$content'; // eslint-disable-line max-len"
-sed -i '2s~.*~'"$output"'~' ext/src/inject.js
+output="module.exports = 'data:$mimetype;base64,$content'; // eslint-disable-line max-len"
+echo $output > src/scripts/overlay-data.js
+echo 'All done!'
